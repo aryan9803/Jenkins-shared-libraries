b7f345448353b941ba809f12e28caefec0e8d4a9
aryan9803 <aryanmirajkar7@gmail.com>
2025-11-26 08:42:01 +0530

    Update login.groovy

--- vars/login.groovy
+++ vars/login.groovy
@@ -2,7 +2,7 @@ def call(String DOCKERHUB_CREDENTIAL_ID)
 { 
   // Uses the defined DOCKERHUB_CREDENTIAL_ID to authenticate
                     withCredentials([usernamePassword(
-                        credentialsId: "$DOCKERHUB_CREDENTIAL_ID", 
+                        credentialsId: "${DOCKERHUB_CREDENTIAL_ID}", 
                         passwordVariable: 'DOCKER_PASSWORD', 
                         usernameVariable: 'DOCKER_USERNAME'
                     )]) {
