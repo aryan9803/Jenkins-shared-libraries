f84edacf2445ce5fcd9806b6bf09470866db7ce4
aryan9803 <aryanmirajkar7@gmail.com>
2025-11-26 08:45:27 +0530

    Create deploy.groovy

---
+++ vars/deploy.groovy
@@ -0,0 +1,12 @@
+ def call(String DOCKER_NAME)
+{
+  echo 'Stopping and removing any old container using port 8000...'
+                sh 'docker stop $(docker ps -q --filter publish=8000) || true'
+                sh 'docker rm $(docker ps -a -q --filter publish=8000) || true'
+                
+                echo 'Starting new container...'
+                // Note: We use the locally built image for deployment here.
+                sh "docker run -d -p 8000:8000 "${DOCKER_NAME}"
+
+                echo 'Deployment Complete' 
+}
