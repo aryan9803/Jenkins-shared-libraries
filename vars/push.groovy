0550d90908726d385a30b0dda7f36679bcc4e9fa
aryan9803 <aryanmirajkar7@gmail.com>
2025-11-26 08:56:02 +0530

    Update push.groovy

--- vars/push.groovy
+++ vars/push.groovy
@@ -1,5 +1,5 @@
 def call(String DOCKERHUB_IMAGE)
 {
-    sh "docker push "${DOCKERHUB_IMAGE}":latest"
-    echo "Image Pushed to Docker Hub: "${DOCKERHUB_IMAGE}":latest"
+    sh "docker push ${DOCKERHUB_IMAGE}:latest"
+    echo "Image Pushed to Docker Hub: ${DOCKERHUB_IMAGE}:latest"
 }
